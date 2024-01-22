4d748438c7533d8423c31628435e277de7bc4f0c
Patrick Lurch <plurch@users.noreply.github.com>
2024-01-22 16:50:49 -0500

    flush print command

--- pl_py_utils/utils.py
+++ pl_py_utils/utils.py
@@ -30,7 +30,7 @@ def timerPrint(msg: str):
       msg (str): The message to be printed alongside the timer's timestamp.
   """
   print(f'(timer: {round(timer())}) - {msg}', flush=True) # https://stackoverflow.com/a/36081434
-  print(get_process_memory_usage())
+  print(get_process_memory_usage(), flush=True)
 
 def max_sublist(l: Sequence[T], max_len: int) -> list[Sequence[T]]:
   """
